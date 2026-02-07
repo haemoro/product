fff0488c9053bc45df2c9ba9d265b71456e3dfba
hamero <pdg1025@gmail.com>
2026-02-08 02:34:53 +0900

    feat: sottiquiz convert api

--- src/main/kotlin/com/sotti/product/configuration/SecurityConfig.kt
+++ src/main/kotlin/com/sotti/product/configuration/SecurityConfig.kt
@@ -5,16 +5,20 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val corsConfigurationSource: CorsConfigurationSource
+) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
+            .cors { it.configurationSource(corsConfigurationSource) }
             .csrf { it.disable() }
-            .authorizeHttpRequests { authz ->
-                authz.anyRequest().permitAll()
+            .authorizeHttpRequests { auth ->
+                auth.anyRequest().permitAll()
             }
         return http.build()
     }
